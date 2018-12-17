9d2ca37300f9ff6860c05a79498a4b573ce44852
branton <937935598@qq.com>
2018-12-17 09:44:47 +0800

    aa

--- project/yanyun/routes/admin.js
+++ project/yanyun/routes/admin.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var fs = require("fs");
+var multiparty = require('multiparty');
 var db = require('../config/db');
 
 // 首页
@@ -57,6 +59,38 @@ router.post('/login/identityKey', function(req, res, next) {
     }
   });
 });
+
+// 文件上传
+router.post('/file_upload',function(req, res,next){
+  //生成multiparty对象，并配置上传目标路径
+  var form = new multiparty.Form({uploadDir: './upload/picture'});
+
+  //上传完成后处理
+  form.parse(req, function(err, fields, files) {
+    var obj ={};
+    var filesTmp = JSON.stringify(files,null,2);
+    if(err){
+      console.log('parse error: ' + err);
+    }else {
+      var inputFile = JSON.parse(filesTmp).file[0];
+      var uploadedPath = inputFile.path;
+      var dstPath = './upload/picture/' + inputFile.originalFilename;
+
+      //重命名为真实文件名
+      fs.rename(uploadedPath, dstPath, function(err) {
+        if(err){
+          console.log('rename error: ' + err);
+          res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+          return res.end("{'status':200, 'message': '上传失败！'}");
+        } else {
+          console.log('rename ok');                
+          res.writeHead(200, {'content-type': 'text/plain;charset=utf-8'});
+          return res.end("{'status':400, 'message': '上传成功！', 'uploadedPath': "+uploadedPath+",'dstPath':"+dstPath+"}");
+        }
+      });
+    }
+  });
+})
 
 // 订单列表
 router.get('/addForm', function(req, res, next) {
